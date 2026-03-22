af23b3a0aaadee241a19d6f19f5506bc68f1e0ca
meAnim <animj.emma@gmail.com>
2026-03-22 08:31:01 +0000

    vault multithreading

--- src/alphaVerse/main.java
+++ src/alphaVerse/main.java
@@ -1,21 +1,21 @@
-package alphaVerse;
-
-public class main {
-
-    public static void main(String[] args) {
-        Thread thread = new PaxHumana();
-
-        System.out.println("we are in the " + Thread.currentThread().getName() + " Thread");
-        thread.start();
-        System.out.println("The thread we created has started");
-
-        System.out.println("we are in the " + Thread.currentThread().getName() + " Thread after starting the new thread");
-
-        try {
-            Thread.sleep(10000);
-        }catch (InterruptedException e){
-            System.out.println(e.getMessage());
-        }
-        
-    }
-}
+//package alphaVerse;
+//
+//public class main {
+//
+//    public static void main(String[] args) {
+//        Thread thread = new PaxHumana();
+//
+//        System.out.println("we are in the " + Thread.currentThread().getName() + " Thread");
+//        thread.start();
+//        System.out.println("The thread we created has started");
+//
+//        System.out.println("we are in the " + Thread.currentThread().getName() + " Thread after starting the new thread");
+//
+//        try {
+//            Thread.sleep(10000);
+//        }catch (InterruptedException e){
+//            System.out.println(e.getMessage());
+//        }
+//
+//    }
+//}
